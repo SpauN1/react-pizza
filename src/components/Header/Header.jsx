cd23dec49bb8d13a541e2182f19ef8bbd147dd81
Spaun1 <andreispaun7@gmail.com>
2023-09-27 04:44:03 +0300

    fix header element

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,23 +11,25 @@ function Header() {
   return (
     <header className="header">
       <div className="container">
-        <a href="#">
-          <div className="header__logo">
-            <img src={logo} alt="логотип пиццы" />
-            <div>
-              <h1>REACT PIZZA</h1>
-              <p>самая вкусная пицца во вселенной</p>
+        <div className="header__content">
+          <a href="#">
+            <div className="header__logo">
+              <img src={logo} alt="логотип пиццы" />
+              <div>
+                <h1>REACT PIZZA</h1>
+                <p>самая вкусная пицца во вселенной</p>
+              </div>
             </div>
-          </div>
-        </a>
-        <Search />
-        <div className="header__basket">
-          <a href="#" className="header__basket-button">
-            <span>0 ₽</span>
-            <div className="header__basket-delimiter"></div>
-            <img src={basket} alt="иконка корзины" />
-            <span>0</span>
           </a>
+          <Search />
+          <div className="header__basket">
+            <a href="#" className="header__basket-button">
+              <span>0 ₽</span>
+              <div className="header__basket-delimiter"></div>
+              <img src={basket} alt="иконка корзины" />
+              <span>0</span>
+            </a>
+          </div>
         </div>
       </div>
     </header>
