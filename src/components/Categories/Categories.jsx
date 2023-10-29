1ec343b0f7a9c94038bb74d8daba7464c93c8f18
Spaun1 <andreispaun7@gmail.com>
2023-10-29 20:33:25 +0300

    add category selection

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Sort from '../Sort/Sort';
 
 import './Categories.scss';
 
 function Categories() {
+  const categories = [
+    'Все',
+    'Мясные',
+    'Вегетарианская',
+    'Гриль',
+    'Острые',
+    'Закрытые',
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <nav className="categories">
       <div className="container">
         <div className="categories__content">
           <ul>
-            <li className="active">Все</li>
-            <li>Мясные</li>
-            <li>Вегетарианская</li>
-            <li>Гриль</li>
-            <li>Острые</li>
-            <li>Закрытые</li>
+            {categories.map((element, index) => (
+              <li
+                onClick={() => onClickCategory(index)}
+                className={activeIndex === index ? 'active' : ''}
+              >
+                {element}
+              </li>
+            ))}
           </ul>
           <Sort />
         </div>
