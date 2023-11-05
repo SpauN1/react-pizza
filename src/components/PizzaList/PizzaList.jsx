469d110fb18bac4040addf0d4d7ab7dab412f7a8
Spaun1 <andreispaun7@gmail.com>
2023-11-06 00:52:24 +0300

    loading data from a fake backend

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import pizzas from '../../assets/pizzas.json';
 import Pagination from '../Pagination/Pagination';
 import PizzaListItem from '../PizzaListItem/PizzaListItem';
 
 import './PizzaList.scss';
 
 function PizzaList() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://65468388fe036a2fa955ca61.mockapi.io/items')
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <main className="main">
       <div className="container">
         <section className="content">
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__product">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaListItem key={obj.id} {...obj} />
             ))}
           </div>
