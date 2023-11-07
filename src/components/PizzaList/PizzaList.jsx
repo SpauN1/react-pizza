035050bd64aac4de65bc539b2b97e538a184612d
Spaun1 <andreispaun7@gmail.com>
2023-11-07 05:08:51 +0300

    add skeleton for pizza

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -2,16 +2,21 @@ import React, { useEffect, useState } from 'react';
 
 import Pagination from '../Pagination/Pagination';
 import PizzaListItem from '../PizzaListItem/PizzaListItem';
+import Skeleton from '../Skeleton/Skeleton';
 
 import './PizzaList.scss';
 
 function PizzaList() {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://65468388fe036a2fa955ca61.mockapi.io/items')
       .then((response) => response.json())
-      .then((data) => setItems(data));
+      .then((data) => {
+        setItems(data);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -20,9 +25,9 @@ function PizzaList() {
         <section className="content">
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__product">
-            {items.map((obj) => (
-              <PizzaListItem key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => <PizzaListItem key={obj.id} {...obj} />)}
           </div>
         </section>
         <Pagination />
