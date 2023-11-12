def6fba46e6d26fd6b15e8e5284d720b911d7da9
Spaun1 <andreispaun7@gmail.com>
2023-11-12 15:14:51 +0300

    add error message

---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import img from '../../assets/error.gif';
+
+function ErrorMessage() {
+  return (
+    <div style={{ padding: '100px' }}>
+      <h2
+        style={{ textAlign: 'center', marginBottom: '15px', fontSize: '20px' }}
+        className="not-found"
+      >
+        Ничего не найдено:
+      </h2>
+      <img
+        style={{
+          display: 'block',
+          width: '250px',
+          height: '250px',
+          objectFit: 'contain',
+          margin: '0 auto',
+        }}
+        src={img}
+        alt="Error"
+      />
+    </div>
+  );
+}
+
+export default ErrorMessage;
