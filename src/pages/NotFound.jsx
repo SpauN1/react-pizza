67cba10739adb1f5bdb1767272c291c552636b90
Spaun1 <andreispaun7@gmail.com>
2023-11-13 08:10:01 +0300

    add not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
+
+function NotFound() {
+  return (
+    <>
+      <ErrorMessage />
+    </>
+  );
+}
+
+export default NotFound;
