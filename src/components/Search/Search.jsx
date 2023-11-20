d8e51a172526b37036042511d19d480402807800
Spaun1 <andreispaun7@gmail.com>
2023-11-20 20:58:18 +0300

    fix: return focus to input after clearing

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -6,14 +6,22 @@ import clearIcon from '../../assets/img/clear-icon.svg';
 import { SearchContext } from '../../context/context';
 
 import './Search.scss';
+import { useRef } from 'react';
 
 function Search() {
   const { searchValue, setSearchValue } = useContext(SearchContext);
+  const inputRef = useRef();
+
+  const onClickClear = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+  };
 
   return (
     <div className="search">
       <img src={search} alt="поиск пиццы" className="search__icon" />
       <input
+        ref={inputRef}
         onChange={(event) => setSearchValue(event.target.value)}
         value={searchValue}
         className="search__input"
@@ -22,7 +30,7 @@ function Search() {
       />
       {searchValue && (
         <img
-          onClick={() => setSearchValue('')}
+          onClick={onClickClear}
           className="search__input-clear"
           src={clearIcon}
           alt="очистить поле ввода"
