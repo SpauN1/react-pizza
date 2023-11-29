43dc64a306d9167b144e6097e0802308f35e7fdd
Spaun1 <andreispaun7@gmail.com>
2023-11-29 06:08:07 +0300

    feat: create cartSlice and add  pizza

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  totalPrice: 0,
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItems: (state, action) => {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        });
+      }
+
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+  },
+});
+
+export const { addItems } = cartSlice.actions;
+
+export default cartSlice.reducer;
