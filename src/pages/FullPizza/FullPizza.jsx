9a1d60a06ee3ab26eda52e844149813fecf81c3f
Spaun1 <andreispaun7@gmail.com>
2024-01-04 14:16:13 +0300

    feat: add fullPizza page

---
+++ src/pages/FullPizza/FullPizza.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+import './FullPizza.scss';
+
+function FullPizza() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [pizza, setPizza] = useState();
+
+  useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get(
+          'https://65468388fe036a2fa955ca61.mockapi.io/items/' + id
+        );
+        setPizza(data);
+      } catch (error) {
+        alert('Ошибка при получении пиццы!');
+        navigate('/');
+      }
+    }
+
+    fetchPizza();
+  }, [id, navigate]);
+
+  if (!pizza) {
+    return 'Загрузка...';
+  }
+
+  return (
+    <div className="full-pizza">
+      <div className="full-pizza__image-container">
+        <img className="full-pizza__img" src={pizza.imageUrl} alt="пицца" />
+      </div>
+      <div className="full-pizza__content">
+        <h2 className="full-pizza__title">{pizza.title}</h2>
+        <p className="full-pizza__description">{pizza.description}</p>
+        <h4 className="full-pizza__price">{pizza.price} ₽</h4>
+        <Link to="/" className="button button__full-pizza">
+          <span>Вернуться назад</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default FullPizza;
