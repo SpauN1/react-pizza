edd7f362048be80078b57a325389715d40e8b477
Spaun1 <andreispaun7@gmail.com>
2024-06-28 06:28:37 +0300

    refactor: code duplication for suspense

--- src/App.tsx
+++ src/App.tsx
@@ -19,29 +19,17 @@ const FullPizza = lazy(
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route path="" element={<Home />} />
-          <Route
-            path="cart"
-            element={
-              <Suspense fallback={<div>Идет загрузка...</div>}>
-                <Cart />
-              </Suspense>
-            }
-          />
-          <Route
-            path="pizza/:id"
-            element={
-              <Suspense fallback={<div>Идет загрузка...</div>}>
-                <FullPizza />
-              </Suspense>
-            }
-          />
-          <Route path="cartempty" element={<CartEmpty />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Идет загрузка...</div>}>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="" element={<Home />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="pizza/:id" element={<FullPizza />} />
+            <Route path="cartempty" element={<CartEmpty />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
